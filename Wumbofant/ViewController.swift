c5598cd4d661a7a63a405a3f9131bf593621da4e
Simon Crequer <sjc291@uclive.ac.nz>
2015-07-13 01:27:27 +1200

    Initial commit

---
+++ Wumbofant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wumbofant
+//
+//  Created by Simon Crequer on 13/07/15.
+//  Copyright (c) 2015 Simon Crequer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
