0d13c30de2ac244f3db964726f579be9a9b8b840
Simon Crequer <sjc291@uclive.ac.nz>
2015-07-14 00:37:45 +1200

    Create LogEntry struct for storing each piece of spent effort

---
+++ WumbofantTests/CSVLoaderTests.swift
@@ -0,0 +1,24 @@
+//
+//  CSVLoaderTests.swift
+//  Wumbofant
+//
+//  Created by Simon Crequer on 13/07/15.
+//  Copyright (c) 2015 Simon Crequer. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class CSVLoaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    
+}
