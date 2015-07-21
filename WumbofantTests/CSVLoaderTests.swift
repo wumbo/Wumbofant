cb19cd2e5b8a761458b15777a9c3fce4dbf43148
Simon Crequer <sjc291@uclive.ac.nz>
2015-07-21 21:09:09 +1200

    Test for last log entry and one with a comma in the comment

--- WumbofantTests/CSVLoaderTests.swift
+++ WumbofantTests/CSVLoaderTests.swift
@@ -65,6 +65,30 @@ class CSVLoaderTests: XCTestCase {
             XCTAssertEqual(expectedDate, entries[0].date)
             XCTAssertEqual(1.0, entries[0].spentEffort)
             
+            // 5271st (last) entry
+            expectedDate = dateFormatter.dateFromString("07.07.2015 15:28")!
+            XCTAssertEqual("6: soft.serve( )", entries[5271].product)
+            XCTAssertEqual("Scrum Machine", entries[5271].project)
+            XCTAssertEqual("3: Sprint Three - soft.serve(Scrum Machine)", entries[5271].iteration)
+            XCTAssertEqual("Story Estimation", entries[5271].story)
+            XCTAssertEqual("Update Story controller", entries[5271].task)
+            XCTAssertEqual("#implement Connected comboboxes to scale and estimation attributes of story #commits[ebb7b87b] #detail Had some trouble with combobox actions being called when setting their initial state causing NullPointerExceptions. Fixed by adding preventCombo boolean #pair[jpn22]", entries[5271].comment)
+            XCTAssertEqual("Simon Crequer", entries[5271].user)
+            XCTAssertEqual(expectedDate, entries[5271].date)
+            XCTAssertEqual(1.0, entries[5271].spentEffort)
+            
+            // 5219th entry (has comma in comment
+            expectedDate = dateFormatter.dateFromString("22.05.2015 16:39")!
+            XCTAssertEqual("6: soft.serve( )", entries[5219].product)
+            XCTAssertEqual("Scrum Machine", entries[5219].project)
+            XCTAssertEqual("3: Sprint Three - soft.serve(Scrum Machine)", entries[5219].iteration)
+            XCTAssertEqual("Revert", entries[5219].story)
+            XCTAssertEqual("BUGFIX: Add cancel button to cancel creation of items", entries[5219].task)
+            XCTAssertEqual("#commits[a6a3a021c] Added cancel button on  creation of any person, team etc. so that creation can be cancelled. #pair[jpn22]]", entries[5219].comment)
+            XCTAssertEqual("Joshua Norton", entries[5219].user)
+            XCTAssertEqual(expectedDate, entries[5219].date)
+            XCTAssertEqual(0.8, entries[5219].spentEffort)
+            
         } else {
             XCTFail("Could not get entries from loader")
         }
